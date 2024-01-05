b625e10e4b3bce5d9de13d5cf146d790fb34d95e
Hannah Nesser <hannah.o.nesser@jpl.nasa.gov>
2024-01-04 16:22:11 -0800

    Added generic satellite parser
    - This parser assumes that the satellite is a netcdf (readable with
      xarray) and that all fields are contained in the main group of the
      file (i.e., the group does not need to be specified). It is likely
      insufficient for most use cases, but can serve as an example of the
      operations needed to be completed by a more specific parser.
    - I also began writing a read_geoschem_file function. Ultimately, this
      should be replaced by a gcpy function.
    - I came across one hiccup: we need to know which files to read when
      so that we are able to match up the satellite and model observation
      times. A few options here:
      - We could add a preprocessing step in which we iterate through all
        the provided satellite files and resave them with "TIMEMIN_TIMEMAX"
        appended to the end of the file names so that we can match it to
        the daily or monthly GEOS-Chem files.
      - We could require that the user provides the satellite file name
        structures (this actually makes some sense regardless) with some
        YYYYMMDD key.
      - Relatedly, we have the issue of the temporal resolution at which to
        apply the operator. Should we do this for daily observations?
        Monthly? Or up to some number of observations? This could also be
        provided as an option in the config file.
      - This also means that we need some sort of create_model_columns.py
        file that actually applies the functions in order (much like your
        example for Sarah)

--- parsers.py
+++ parsers.py
@@ -1,11 +1,44 @@
-from gcpy import read_geoschem_file
+import xarray as xr
+import yaml
+# from gcpy import read_geoschem_file
 
-with open("config.yml", "r", encoding="utf8") as f:
+with open("config.yaml", "r", encoding="utf8") as f:
     config = yaml.safe_load(f)
 
+def read_geoschem_file(file_path):
+    '''
+    Eventually, this should be switched to a gcpy function. 
+    From Elise:
+        read_gc_file = read_geoschem_file # use gcpy function for reading GEOS-Chem files, may need to wrap
+    '''
 
-read_gc_file = read_geoschem_file # use gcpy function for reading GEOS-Chem files, may need to wrap
 
+def read_satellite_file(file_path, satellite_name):
+    '''
+    This generic parser assumes that the data is a netcdf with a single, 
+    main group with variables as defined in the config.yaml file. It 
+    also assumes that all filtering is contained in an optional quality_flag
+    file that is 0 for quality data and 1 elsewhere. In reality, satellite
+    data may be contained in complex netcdf files with multiple groups and 
+    may require filtering along multiple criteria. Please write your own 
+    parser in these cases.
+    '''
+    # Get the fields for the satellite specified as defined in config.yaml
+    fields = config[satellite_name]["DATA_FIELDS"]
+    
+    # Remove quality_flag if it isn't present in the fields
+    fields = {k : v for k, v in fields.items() if v.lower() != 'none'}
+
+    # Open the file and subset
+    satellite = xr.open_dataset(file_path)[list(fields.values())]
+
+    # Rename satellite dimension names to the standard (again, as defined in
+    # config.yaml)
+    rename_fields = {v : k for k, v in fields.items()}
+    satellite = satellite.rename(rename_fields)
+
+    # Return the data
+    return satellite
 
 def read_TROPOMI_vXX(file_path):
     # read TROPOMI file
@@ -16,4 +49,8 @@ def read_TROPOMI_vXX(file_path):
 def read_GOSAT_vXX(file_path):
     # read GOSAT file
     # grab tropomic data columns specified in config and rename them to standard naming
+    pass
+
+
+def read_OCO2(file_path):
     pass
