bff2ad834b0e9aea4cce337bb4e4657604eec342
Hannah Nesser <hannah.o.nesser@jpl.nasa.gov>
2024-01-05 19:17:28 -0800

    Added read_geoschem_file function, and standardized  format of satellite reading functions
    - Added a function to read GEOSChem files containing pressure and concentration
      information, merge them together, and change variable names to the standard
      format defined in config.yaml
    - Added satellite_name variable (along with default) for all satellite parsers
      currently included in the file.

--- parsers.py
+++ parsers.py
@@ -1,16 +1,40 @@
 import xarray as xr
 import yaml
-# from gcpy import read_geoschem_file
+# from gcpy import read_geos_chem_file
 
 with open("config.yaml", "r", encoding="utf8") as f:
     config = yaml.safe_load(f)
 
-def read_geoschem_file(file_path):
+
+def _open_geoschem(file_path, variables):
+    preprocess = lambda ds : ds[variables]
+    return xr.open_mfdataset(file_path, preprocess=preprocess)
+
+
+def read_geoschem_file(file_path_edges, file_path_conc):
     '''
     Eventually, this should be switched to a gcpy function. 
     From Elise:
         read_gc_file = read_geoschem_file # use gcpy function for reading GEOS-Chem files, may need to wrap
     '''
+    # Define the variables that should be maintained when opening the files
+    fields = config["MODEL"]["DATA_FIELDS"]
+
+    conc_vars = dict(fields)
+    del conc_vars["PRESSURE_EDGES"]
+
+    edge_vars = dict(fields)
+    del edge_vars["CONCENTRATION_AT_PRESSURE_CENTERS"]
+
+    # Open and combine edge and concentration files
+    gc = xr.merge([_open_geoschem(file_path_conc, list(conc_vars.values())),
+                   _open_geoschem(file_path_edges, list(edge_vars.values()))])
+
+    # Rename the fields to the standard (as defined in config.yaml)
+    rename_fields = {v : k for k, v in fields.items()}
+    gc = gc.rename(rename_fields)
+
+    return gc
 
 
 def read_satellite_file(file_path, satellite_name):
@@ -32,7 +56,7 @@ def read_satellite_file(file_path, satellite_name):
     # Open the file and subset
     satellite = xr.open_dataset(file_path)[list(fields.values())]
 
-    # Rename satellite dimension names to the standard (again, as defined in
+    # Rename satellite dimension names to the standard (as defined in 
     # config.yaml)
     rename_fields = {v : k for k, v in fields.items()}
     satellite = satellite.rename(rename_fields)
@@ -40,17 +64,17 @@ def read_satellite_file(file_path, satellite_name):
     # Return the data
     return satellite
 
-def read_TROPOMI_vXX(file_path):
+def read_TROPOMI_vXX(file_path, satellite_name="TROPOMI_vXX"):
     # read TROPOMI file
     # grab tropomi data columns specified in config and rename them to standard naming
     pass
 
 
-def read_GOSAT_vXX(file_path):
+def read_GOSAT_vXX(file_path, satellite_name="GOSAT_vXX"):
     # read GOSAT file
     # grab tropomic data columns specified in config and rename them to standard naming
     pass
 
 
-def read_OCO2(file_path):
+def read_OCO2(file_path, satellite_name="OCO2"):
     pass
