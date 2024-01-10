29811c7ce53d87561c90c882b2f8a8c9d2ab3b77
Hannah Nesser <hannah.o.nesser@jpl.nasa.gov>
2024-01-09 17:32:06 -0800

    Edits to parsers.py - Added if statement to check satellite pressure_edge direction,   and to switch the direction of all vertically-resolved data if   they are defined from TOA instead of surface. - Add read_OCO2_v11_1_preprocessed function to deal with units on   satellite data (from ppm to mol/mol) - Added check_satellite_data function that should be used to check   basic structure of satellite files once read in. This is   by default combined with the chosen satellite parser in the   utilities get_parser (or something like that) function.

--- parsers.py
+++ parsers.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import numpy as np
 import yaml
 # from gcpy import read_geos_chem_file
 
@@ -11,11 +12,12 @@ def _open_geoschem(file_path, variables):
     return xr.open_mfdataset(file_path, preprocess=preprocess)
 
 
-def read_geoschem_file(file_path_edges, file_path_conc):
+def read_geoschem_file(file_path_conc, file_path_edges):
     '''
     Eventually, this should be switched to a gcpy function. 
     From Elise:
-        read_gc_file = read_geoschem_file # use gcpy function for reading GEOS-Chem files, may need to wrap
+        read_gc_file = read_geoschem_file 
+        # use gcpy function for reading GEOS-Chem files, may need to wrap
     '''
     # Define the variables that should be maintained when opening the files
     fields = config["MODEL"]["DATA_FIELDS"]
@@ -24,7 +26,7 @@ def read_geoschem_file(file_path_edges, file_path_conc):
     del conc_vars["PRESSURE_EDGES"]
 
     edge_vars = dict(fields)
-    del edge_vars["CONCENTRATION_AT_PRESSURE_CENTERS"]
+    del edge_vars["CONC_AT_PRESSURE_CENTERS"]
 
     # Open and combine edge and concentration files
     gc = xr.merge([_open_geoschem(file_path_conc, list(conc_vars.values())),
@@ -33,6 +35,11 @@ def read_geoschem_file(file_path_edges, file_path_conc):
     # Rename the fields to the standard (as defined in config.yaml)
     rename_fields = {v : k for k, v in fields.items()}
     gc = gc.rename(rename_fields)
+    
+    # Transpose
+    gc = gc.transpose("TIME", "LONGITUDE", "LATITUDE", "LEV", "ILEV")
+
+    #  TO DO: Check if we need to fill the first hour of the data.
 
     return gc
 
@@ -61,12 +68,23 @@ def read_satellite_file(file_path, satellite_name):
     rename_fields = {v : k for k, v in fields.items()}
     satellite = satellite.rename(rename_fields)
 
+    # Ensure that satellite pressure levels are in descending order:
+    if np.all(np.diff(satellite["PRESSURE_EDGES"]) > 0):
+        print("Switching direction of N_EDGES.")
+        satellite = satellite.reindex(
+            N_EDGES=list(reversed(satellite["N_EDGES"]))
+        )
+
     # Return the data
     return satellite
 
-def read_TROPOMI_vXX(file_path, satellite_name="TROPOMI_vXX"):
+def read_TROPOMI_vXX_science(file_path, satellite_name="TROPOMI_vXX"):
     # read TROPOMI file
-    # grab tropomi data columns specified in config and rename them to standard naming
+    # grab tropomi data columns specified in config and rename them to 
+    # standard naming
+    # First pass of a TROPOMI science product parser (without using any 
+    # TROPOMI data)
+
     pass
 
 
@@ -76,5 +94,20 @@ def read_GOSAT_vXX(file_path, satellite_name="GOSAT_vXX"):
     pass
 
 
-def read_OCO2(file_path, satellite_name="OCO2"):
+def read_OCO2_v11_1_preprocessed(file_path, 
+                                 satellite_name="OCO2_v11.1_preprocessed"):
+    # Use the standard parser first
+    satellite = read_satellite_file(file_path, satellite_name)
+    
+    # Convert units from ppm to mol/mol
+    satellite["PRIOR_PROFILE"] *= 1e-6
+    satellite["SATELLITE_COLUMN"] *= 1e-6
+
+    return satellite
+
+
+def check_satellite_data(dataset):
+    # TO DO: assert type(sat["TIME"]) == datetime (need to deal with the fact
+    # that dtype shows up as '<M8[ns]' or '>M8[ns]' depending on the endianess
+    # ?? of the system)
     pass
