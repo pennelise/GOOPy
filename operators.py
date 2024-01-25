d9e570c713ffc939045b6a91e7f0deec30462193
Hannah Nesser <hannah.o.nesser@jpl.nasa.gov>
2024-01-25 10:33:52 -0800

    Changing MODEL_COLUMNS to MODEL_COLUMN, consistent with the language used for satellites

--- operators.py
+++ operators.py
@@ -112,7 +112,7 @@ def apply_operator_to_chunks(model_conc_files,
     # Concatenate together, combine, and return
     if len(model_columns) > 0:
         model_columns = xr.concat(model_columns, dim="N_OBS")
-        model_columns = model_columns.rename("MODEL_COLUMNS")
+        model_columns = model_columns.rename("MODEL_COLUMN")
         if config[satellite_name]["SAVE_SATELLITE_DATA"].lower() == "true":
             satellite_columns = xr.concat(satellite_columns, dim="N_OBS")
             model_columns = xr.merge([model_columns, satellite_columns])
