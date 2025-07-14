007d8dfd271ff980de24f96fa2c93ad457dc0dd1
Elise Penn <eliserpenn@gmail.com>
2025-07-14 09:51:53 -0400

    Cleaned up comments in main.py. Moved most details to the docstring and removed comments where code is self-explanatory.

--- main.py
+++ main.py
@@ -11,14 +11,26 @@ def apply_operator_to_chunks(model_conc_files,
                              model_edge_files,
                              satellite, 
                              config):
-    # We iterate through this in chunks of
-    # file_length_threshold to balance memory constraints with the
-    # benefits of vectorization.
+    """ 
+    Applies the operator in chunks of file_length_threshold to balance 
+    memory constraints with the benefits of vectorization.
+
+    Inputs:
+        model_conc_files: list of model concentration filepaths
+        model_edge_files: list of model edge filepaths.
+        satellite: xarray dataset with satellite data, 
+                   output from satellite parser in parsers.py
+        config: dictionary with configuration settings
+    
+    Returns:
+        xarray dataset with model columns and satellite columns.
+    """
+
     i = 0
     model_columns = []
     satellite_columns = []
     while i < satellite.dims["N_OBS"]:
-        # Subset the satellite data
+
         sat_i = satellite.isel(
             N_OBS=slice(
                 int(i), 
@@ -26,17 +38,13 @@ def apply_operator_to_chunks(model_conc_files,
             )
         )
     
-        # Get the dates that need to be processed
         process_dates = np.unique(sat_i["TIME"].dt.strftime("%Y-%m-%d"))
 
-        # Load the model data for those dates
         mod_i = parsers.read_geoschem_file(
             util.get_gc_files_for_dates(model_conc_files, process_dates),
             util.get_gc_files_for_dates(model_edge_files, process_dates),
             config["MODEL"]["DATA_FIELDS"])
 
-        # Check for times that are missing in the satellite data and continue
-        # if there are no overlapping itmes
         missing_times = util.get_missing_times(sat_i["TIME"], mod_i["TIME"])
         if (~missing_times).sum() == 0:
             print("  There are no overlapping satellite and model data in"
@@ -54,7 +62,6 @@ def apply_operator_to_chunks(model_conc_files,
                 sat_i.drop(['PRESSURE_EDGES', 'PRESSURE_WEIGHT',
                             'AVERAGING_KERNEL', 'PRIOR_PROFILE', 'N_EDGES']))
 
-        # Step up i
         i += config["LOCAL_SETTINGS"]["FILE_LENGTH_THRESHOLD"]
 
     # Concatenate together, combine, and return
@@ -69,37 +76,41 @@ def apply_operator_to_chunks(model_conc_files,
         return None
 
 
-def apply_operator(config):#satellite_name, file_length_threshold=1e6):
-    """apply one of the default operators to a satellite"""
+def apply_operator(config):
+    """ Apply one of the default satellite operators to a model profile. 
+    
+    Inputs:
+        config: dictionary with configuration settings.
+        
+    Returns:
+        Saves an xarray dataset with model columns and satellite
+        data to a netcdf file in the directory specified by 
+        config["LOCAL_SETTINGS"]["SAVE_DIR"]. 
+    
+    """
 
     # Make save out directory
     if not os.path.exists(config["LOCAL_SETTINGS"]["SAVE_DIR"]):
         os.makedirs(config["LOCAL_SETTINGS"]["SAVE_DIR"])
 
-    # Obtain a list of the satellite and GEOS-Chem files.
     files = util.get_file_lists(config["LOCAL_SETTINGS"])
     satellite_files, model_edge_files, model_conc_files = files
 
     # Get the dates for which we have model files.
-    # TO DO: Currently, this assumes that the GEOS-Chem files are daily or
-    # monthly. We should update this to be more flexible.
+        # TO DO: Currently, this assumes that the GEOS-Chem files are daily or
+        # monthly. We should update this to be more flexible.
     model_dates = np.unique(
         [date for date in util.get_gc_dates(model_edge_files)
          if date in util.get_gc_dates(model_conc_files)])
 
-    # Get the satellite parser. 
     read_satellite = util.get_satellite_parser(config)
 
-    # Iterate through the satellite files:
     for sf in satellite_files:
         short_name = sf.split("/")[-1]
 
-        # Read the first file
         print(f"Processing {short_name}")
-        satellite = read_satellite(sf)
+        satellite = read_satellite(sf) # Read the first file
 
-        # Get unique dates from the file that overlap with the model dates
-        # and subset for those dates.
         satellite_dates = [
             date for date 
             in np.unique(satellite["TIME"].dt.strftime("%Y-%m-%d"))
@@ -115,11 +126,9 @@ def apply_operator(config):#satellite_name, file_length_threshold=1e6):
             satellite["TIME"].dt.strftime("%Y-%m-%d").isin(satellite_dates), 
             drop=True)
 
-        # Next, apply the operator
         model_columns = apply_operator_to_chunks(
             model_conc_files, model_edge_files, satellite, config)
         
-        # Save
         if model_columns is not None:
             short_name = short_name.split('.')[0] + '_operator.nc'
             model_columns.to_netcdf(
